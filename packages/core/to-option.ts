c96de0dd8613592f8e3e1538937d82fc525c7eb0
Blake Mealey <blake.mealey@vercel.com>
2022-12-21 14:10:30 -0600

    delete core package

--- packages/core/to-option.ts
+++
@@ -1,9 +0,0 @@
-import { None, Some } from './option';
-import { Value } from './value';
-
-export function toOption<T extends Value>(value: T | undefined | null) {
-  if (value === undefined || value === null) {
-    return None<T>();
-  }
-  return Some(value);
-}
--- packages/core/value.ts
+++
@@ -1,3 +0,0 @@
-export const Void = Symbol('NoError(Void)');
-
-export type Value = {} | typeof Void | unknown;
